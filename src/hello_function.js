e143faf30ae4cec8077ece8e999706c0e0a214a9
Maneesh Mehra <maneeshmehra@users.noreply.github.com>
2018-07-09 15:08:01 -0400

    Update hello_function.js

--- src/hello_function.js
+++ src/hello_function.js
@@ -1,11 +1,11 @@
 'use strict';
 
 function main(params) {
+   console.log('Hello function called');
    var greeting = params.greeting;
    var name = params.name;
    var message = greeting + ", " + name;
 
-   console.log('Hello function called');
    console.log('Greeting: ' + greeting);
    console.log('Name: ' + name);
 
