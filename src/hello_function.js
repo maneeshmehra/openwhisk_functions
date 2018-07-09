dd4c8fa42f2df8568613439b04107618bb69b5c3
Maneesh Mehra <maneeshmehra@users.noreply.github.com>
2018-07-09 15:08:23 -0400

    Update hello_function.js

--- src/hello_function.js
+++ src/hello_function.js
@@ -4,11 +4,11 @@ function main(params) {
    console.log('Hello function called');
    var greeting = params.greeting;
    var name = params.name;
-   var message = greeting + ", " + name;
 
    console.log('Greeting: ' + greeting);
    console.log('Name: ' + name);
 
+   var message = greeting + ", " + name;
    return {
       "Message": message,
       "Status": "Success",
